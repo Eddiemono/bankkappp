f5637a092f9c3f2e02fc81c3a85aed287292eb16
Eddiemono <142228771+Eddiemono@users.noreply.github.com>
2024-11-13 15:17:45 +0000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: '*',
+  origin: https://blubankofamerica.onrender.com, '*',
 }));
 app.use(cookieParser());
 app.use(bodyParser.json());
